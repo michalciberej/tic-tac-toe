7bdbb4c2ec80e9f68ade65916dd9cd326c5af7a6
Michal Ciberej <michal.ciberej@gmail.com>
2023-06-09 16:58:48 +0200

    Create Ui layout

---
+++ script.js
@@ -0,0 +1,10 @@
+// ! create Ui that shows 9 divs as a box display player names and in the end of the game display who won, create resetGame button 
+// ? add function to swap playermode if player1 has more tiles then player2 and the oter way 
+// * give every boardgame element exact position in the array. Start with array of 0-8. reasign exact element's value to chosen symbol onclick 
+// ! add function that checks for a pattern of symbols in the array e.g. arr[0,3,6] (I know that doesnt work but it's good for me to visualize) === "X" means win. Also is a vertical line on the left side of the board etc. 
+// ? array.lenght == 8 && all other pattern checking functions return false - display draw. display the resetGame button 
+// * add factory function that creates objects of player with name, X/O symbol 
+// ! add module for functions that checks for a patterns of symbols in array. 
+// ? create resetGame() that resets symbols in the array to 0-8, keep names of players and resets dom
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
